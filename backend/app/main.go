ac29263c451f53fe99469e9d0cf2db8e2b06da13
yano-kentaro <ynkn0829@gmail.com>
2022-05-06 16:22:27 +0900

    メイン処理

--- backend/app/main.go
+++ backend/app/main.go
@@ -17,18 +17,15 @@
 package main
 
 import (
+	"fmt"
+	"fullcalendar/handler"
+	"fullcalendar/lib"
+	"fullcalendar/models/events"
 	"os"
+	"time"
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
-)
-
-//===================================================|0
-//                    グローバル変数
-//==========================================|2022_05_04
-
-var (
-	FRONT_URL = os.Getenv("FRONT_URL")
 )
 
 //===================================================|0
@@ -36,11 +33,46 @@ var (
 //==========================================|2022_05_04
 
 func main() {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
-	r.Run()
+	//------------------------------
+	// DB接続＋終了時切断
+	lib.DBOpen()
+	defer lib.DBClose()
+
+	//------------------------------
+	// APIサーバー設定
+	r := gin.Default() // r <- router
+	r.Use(cors.New(cors.Config{
+		AllowOrigins: []string{
+			"http://localhost",
+		},
+		AllowMethods: []string{
+			"GET", "POST", "PUT", "DELETE", "OPTIONS",
+		},
+		AllowHeaders: []string{
+			"Access-Control-Allow-Credentials",
+			"Access-Control-Allow-Headers",
+			"Content-Type",
+			"Content-Length",
+			"Accept-Encoding",
+			"Authorization",
+		},
+		AllowCredentials: true,
+		MaxAge:           24 * time.Hour,
+	}))
+
+	//------------------------------
+	// Events
+	events := events.New()
+	r.GET("/events", handler.GetEvents(events))
+	r.POST("/event/create", handler.CreateEvent(events))
+	r.PUT("/event/update/:id", handler.UpdateEvent(events))
+	r.DELETE("event/delete/:id", handler.DeleteEvent(events))
+
+	//------------------------------
+	// APIサーバー起動
+	httpHost := os.Getenv("HTTP_HOST")
+	httpPort := os.Getenv("HTTP_PORT")
+	r.Run(fmt.Sprintf(
+		"%[1]s:%[2]s", httpHost, httpPort,
+	))
 }
