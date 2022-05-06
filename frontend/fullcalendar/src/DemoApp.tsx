7686ca92cd9f95ff8f0da07f20a987dd2886111b
yano-kentaro <ynkn0829@gmail.com>
2022-05-06 16:22:38 +0900

    フロント実装

--- frontend/fullcalendar/src/DemoApp.tsx
+++ frontend/fullcalendar/src/DemoApp.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
-import FullCalendar, { EventApi, DateSelectArg, EventClickArg, EventContentArg, formatDate } from '@fullcalendar/react'
+import FullCalendar, { EventApi,
+    DateSelectArg,EventClickArg,
+    EventContentArg, formatDate, EventDropArg
+  } from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid'
 import timeGridPlugin from '@fullcalendar/timegrid'
-import interactionPlugin from '@fullcalendar/interaction'
+import interactionPlugin, { EventResizeDoneArg } from '@fullcalendar/interaction'
 import { INITIAL_EVENTS, createEventId } from './event-utils'
+import axios from 'axios'
 
 interface DemoAppState {
   weekendsVisible: boolean
@@ -20,7 +24,7 @@ export default class DemoApp extends React.Component<{}, DemoAppState> {
   render() {
     return (
       <div className='demo-app'>
-        {this.renderSidebar()}
+        {/* {this.renderSidebar()} */}
         <div className='demo-app-main'>
           <FullCalendar
             plugins={[
@@ -34,25 +38,12 @@ export default class DemoApp extends React.Component<{}, DemoAppState> {
               right: 'dayGridMonth,timeGridWeek,timeGridDay'
             }}
             events={{
-              url: "http://localhost:8000/todos/"
+              url: "http://localhost:3000/events"
             }}
-            eventSourceSuccess={
-              function(content, xhr){
-                content.forEach(event => {
-                  event.id = event.ID
-                  event.title = event.Title
-                  event.start = event.Start
-                  event.end = event.End
-                  delete event.ID
-                  delete event.TITLE
-                  delete event.START
-                  delete event.END
-                });
-                return content
-              }
-            }
             initialView='dayGridMonth'
             editable={true}
+            eventResize={this.eventResize}
+            eventDrop={this.eventDrop}
             selectable={true}
             selectMirror={true}
             dayMaxEvents={true}
@@ -73,36 +64,36 @@ export default class DemoApp extends React.Component<{}, DemoAppState> {
     )
   }
 
-  renderSidebar() {
-    return (
-      <div className='demo-app-sidebar'>
-        <div className='demo-app-sidebar-section'>
-          <h2>Instructions</h2>
-          <ul>
-            <li>Select dates and you will be prompted to create a new event</li>
-            <li>Drag, drop, and resize events</li>
-            <li>Click an event to delete it</li>
-          </ul>
-        </div>
-        <div className='demo-app-sidebar-section'>
-          <label>
-            <input
-              type='checkbox'
-              checked={this.state.weekendsVisible}
-              onChange={this.handleWeekendsToggle}
-            ></input>
-            toggle weekends
-          </label>
-        </div>
-        <div className='demo-app-sidebar-section'>
-          <h2>All Events ({this.state.currentEvents.length})</h2>
-          <ul>
-            {this.state.currentEvents.map(renderSidebarEvent)}
-          </ul>
-        </div>
-      </div>
-    )
-  }
+  // renderSidebar() {
+  //   return (
+  //     <div className='demo-app-sidebar'>
+  //       <div className='demo-app-sidebar-section'>
+  //         <h2>Instructions</h2>
+  //         <ul>
+  //           <li>Select dates and you will be prompted to create a new event</li>
+  //           <li>Drag, drop, and resize events</li>
+  //           <li>Click an event to delete it</li>
+  //         </ul>
+  //       </div>
+  //       <div className='demo-app-sidebar-section'>
+  //         <label>
+  //           <input
+  //             type='checkbox'
+  //             checked={this.state.weekendsVisible}
+  //             onChange={this.handleWeekendsToggle}
+  //           ></input>
+  //           toggle weekends
+  //         </label>
+  //       </div>
+  //       <div className='demo-app-sidebar-section'>
+  //         <h2>All Events ({this.state.currentEvents.length})</h2>
+  //         <ul>
+  //           {this.state.currentEvents.map(renderSidebarEvent)}
+  //         </ul>
+  //       </div>
+  //     </div>
+  //   )
+  // }
 
   handleWeekendsToggle = () => {
     this.setState({
@@ -110,26 +101,55 @@ export default class DemoApp extends React.Component<{}, DemoAppState> {
     })
   }
 
-  handleDateSelect = (selectInfo: DateSelectArg) => {
+  // 新規登録
+  handleDateSelect = async (selectInfo: DateSelectArg) => {
     let title = prompt('Please enter a new title for your event')
     let calendarApi = selectInfo.view.calendar
 
     calendarApi.unselect() // clear date selection
 
-    if (title) {
-      calendarApi.addEvent({
-        id: createEventId(),
-        title,
+    if(title) {
+      const res = await axios.post('http://localhost:3000/event/create', {
+        title: title,
         start: selectInfo.startStr,
         end: selectInfo.endStr,
-        allDay: selectInfo.allDay
       })
+      calendarApi.refetchEvents()
+    } else {
+      alert("title is required.")
     }
   }
 
-  handleEventClick = (clickInfo: EventClickArg) => {
+  // 予定期間変更
+  eventResize = async (resizeInfo: EventResizeDoneArg) => {
+    let calendarApi = resizeInfo.view.calendar
+    let newEvent = resizeInfo.event
+    const res = await axios.put('http://localhost:3000/event/update/' + newEvent.id, {
+      title: newEvent.title,
+      start: newEvent.startStr,
+      end: newEvent.endStr
+    })
+    calendarApi.refetchEvents()
+  }
+
+  // 予定開始時刻変更
+  eventDrop = async (dropInfo: EventDropArg) => {
+    let calendarApi = dropInfo.view.calendar
+    let newEvent = dropInfo.event
+    const res = await axios.put('http://localhost:3000/event/update/' + newEvent.id, {
+      title: newEvent.title,
+      start: newEvent.startStr,
+      end: newEvent.endStr
+    })
+    calendarApi.refetchEvents()
+  }
+
+  // 削除
+  handleEventClick = async (clickInfo: EventClickArg) => {
     if (confirm(`Are you sure you want to delete the event '${clickInfo.event.title}'`)) {
-      clickInfo.event.remove()
+      let calendarApi = clickInfo.view.calendar
+      const res = await axios.delete('http://localhost:3000/event/delete/' + clickInfo.event.id)
+      calendarApi.refetchEvents()
     }
   }
 
@@ -150,11 +170,11 @@ function renderEventContent(eventContent: EventContentArg) {
   )
 }
 
-function renderSidebarEvent(event: EventApi) {
-  return (
-    <li key={event.id}>
-      <b>{formatDate(event.start!, {year: 'numeric', month: 'short', day: 'numeric'})}</b>
-      <i>{event.title}</i>
-    </li>
-  )
-}
+// function renderSidebarEvent(event: EventApi) {
+//   return (
+//     <li key={event.id}>
+//       <b>{formatDate(event.start!, {year: 'numeric', month: 'short', day: 'numeric'})}</b>
+//       <i>{event.title}</i>
+//     </li>
+//   )
+// }
